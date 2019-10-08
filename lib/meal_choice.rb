3b3483cbd1fa7d5b9b9954d6986b5cf29c0ce5a3
Arya Ziai <arya721@gmail.com>
2019-10-08 22:27:09 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,5 +2,5 @@
 
 def meal_choice(veg1, protein = "meat" , veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
