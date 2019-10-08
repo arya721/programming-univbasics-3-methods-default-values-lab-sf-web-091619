29ab443b9ee34f6d1139ebfed859cac34c9dc95d
Arya Ziai <arya721@gmail.com>
2019-10-08 22:23:39 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,7 +1,11 @@
-# Your code here
+  def meal_choice
 
-def meal_choice(veg1, protein = "meat" , veg2)
-  puts "A plate of #{protein} with #{veg1} and #{veg2}"
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
-else 
-end
+
+
+
+
+
+
+
+
+  end
