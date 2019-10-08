2aae42617b2e89eb8e5a05bd5fdf1aa975c2dce6
Arya Ziai <arya721@gmail.com>
2019-10-08 22:35:08 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,9 @@
 # Your code here
 
 def meal_choice(veg1="broccol", protein = "meat" , veg2="macaroni")
+
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}." so that you can also see what you've ordered.
+
   print "A plate of #{protein} with #{veg1} and #{veg2}"
 end
